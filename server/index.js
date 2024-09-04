ae14258e97bd07abb38facfc0c17db109104c414
Yamini-toto <yamijoahi99@gmail.com>
2024-09-05 00:24:06 +0530

    fixing cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const socketIO = require("socket.io");
 const app = express();
 const users =[];
 app.use(cors({
-  origin: 'https://chit-chat-1-tf55.onrender.com/chat'
+  origin: 'https://chit-chat-1-tf55.onrender.com'
 }));
 
 const port = 4500 || process.env.port;
